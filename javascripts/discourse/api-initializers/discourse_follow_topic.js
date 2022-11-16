47eac4ea67732984b8071043973582285b9b9749
Ahmed Gagan <ahmedgagan6@gmail.com>
2022-11-16 17:42:30 +0530

    DEV: prettier

--- javascripts/discourse/api-initializers/discourse_follow_topic.js
+++ javascripts/discourse/api-initializers/discourse_follow_topic.js
@@ -1,22 +1,21 @@
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("0.11.1", api => {
-  api.includePostAttributes('topic');
+export default apiInitializer("0.11.1", (api) => {
+  api.includePostAttributes("topic");
 
-  api.reopenWidget('post-menu', {
+  api.reopenWidget("post-menu", {
     toggleNotificationLevel() {
       const { attrs } = this;
 
       const level = attrs.topic.details.notification_level != 3 ? 3 : 1;
 
-      attrs.topic.details
-        .updateNotifications(level).then(() => {
-          this.appEvents.trigger("post-stream:refresh", { id: attrs.id })
-        });
-    }
+      attrs.topic.details.updateNotifications(level).then(() => {
+        this.appEvents.trigger("post-stream:refresh", { id: attrs.id });
+      });
+    },
   });
 
-  api.addPostMenuButton('followTopic', (attrs) => {
+  api.addPostMenuButton("followTopic", (attrs) => {
     if (!attrs.firstPost) {
       return;
     }
@@ -26,11 +25,14 @@ export default apiInitializer("0.11.1", api => {
     }
 
     return {
-      action: 'toggleNotificationLevel',
-      icon: attrs.topic.details.notification_level != 3 ? 'far-bell': 'bell',
-      className: 'follow-topic',
-      title: attrs.topic.details.notification_level != 3 ? 'topics.follow': 'topics.unfollow',
-      position: 'first'
+      action: "toggleNotificationLevel",
+      icon: attrs.topic.details.notification_level != 3 ? "far-bell" : "bell",
+      className: "follow-topic",
+      title:
+        attrs.topic.details.notification_level != 3
+          ? "topics.follow"
+          : "topics.unfollow",
+      position: "first",
     };
   });
 });
